e1bf5823abfec9c3ee05b5c1293c1a0b0a1063e6
Rusher <105472000+rusherdv@users.noreply.github.com>
2022-08-20 20:34:58 -0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,40 @@
+Config = {}
+
+Config.mafia01 = "" --+ DISCORD ROL ID +--
+Config.mafia02 = "" --+ DISCORD ROL ID +--
+Config.mafia03 = "" --+ DISCORD ROL ID +--
+Config.mafia04 = "" --+ DISCORD ROL ID +--
+Config.mafia05 = "" --+ DISCORD ROL ID +--
+Config.mafia06 = "" --+ DISCORD ROL ID +--
+Config.mafia07 = "" --+ DISCORD ROL ID +--
+Config.mafia08 = "" --+ DISCORD ROL ID +--
+Config.mafia09 = "" --+ DISCORD ROL ID +--
+Config.mafia10 = "" --+ DISCORD ROL ID +--
+Config.mafia11 = "" --+ DISCORD ROL ID +--
+Config.mafia12 = "" --+ DISCORD ROL ID +--
+Config.mafia13 = "" --+ DISCORD ROL ID +--
+Config.mafia14 = "" --+ DISCORD ROL ID +--
+Config.mafia15 = "" --+ DISCORD ROL ID +--
+Config.mafia16 = "" --+ DISCORD ROL ID +--
+Config.mafia17 = "" --+ DISCORD ROL ID +--
+Config.mafia18 = "" --+ DISCORD ROL ID +--
+Config.mafia19 = "" --+ DISCORD ROL ID +--
+Config.mafia20 = "" --+ DISCORD ROL ID +--
+Config.jefe = "" --+ DISCORD ROL ID +--
+
+Config.banda01 = "" --+ DISCORD ROL ID +--
+Config.banda02 = "" --+ DISCORD ROL ID +--
+Config.banda03 = "" --+ DISCORD ROL ID +--
+Config.banda04 = "" --+ DISCORD ROL ID +--
+Config.banda05 = "" --+ DISCORD ROL ID +--
+
+Config.mecanico = "" --+ DISCORD ROL ID +--
+Config.policia = "" --+ DISCORD ROL ID +--
+Config.same = "" --+ DISCORD ROL ID +--
+
+Config.helper = "" --+ DISCORD ROL ID +--
+Config.mod = "" --+ DISCORD ROL ID +--
+Config.admin = "" --+ DISCORD ROL ID +--
+Config.manager = "" --+ DISCORD ROL ID +--
+Config.encargado = "" --+ DISCORD ROL ID +--
+
